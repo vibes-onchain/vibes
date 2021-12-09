f1e9f4a57b62e3731a69a0e34c56c07730a06d00
Foy Savas <foy@sav.as>
2021-12-08 21:58:02 -0500

    redirect to discord install

--- vibes-site/src/routes/setup/discord/index.js
+++ vibes-site/src/routes/setup/discord/index.js
@@ -6,19 +6,24 @@ import { Link } from "react-router-dom";
 import Footer from ":/components/Footer";
 import Header from ":/components/Header";
 
+const DISCORD_BOT_URL =
+  "https://discord.com/api/oauth2/authorize?client_id=908434595614183457&permissions=8&scope=bot";
 
 export default function () {
-  return <div css={CSS}>
-    <Header />
-    <center>
-    <h1>Get the Vibes Discord Bot</h1>
-    </center>
-    <Footer />
-  </div>;
+  window.location.href = DISCORD_BOT_URL;
+  return (
+    <div css={CSS}>
+      <Header />
+      <center>
+        <h1>Get the Vibes Discord Bot</h1>
+      </center>
+      <Footer />
+    </div>
+  );
 }
 
 const CSS = css`
-center h1 {
-  margin: 20px auto;
-}
+  center h1 {
+    margin: 20px auto;
+  }
 `;
