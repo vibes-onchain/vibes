7e8323e2ee0c760fa24590ffff4d432b5551cdaa
pepimartinez <pepimartinez@protonmail.com>
2021-12-08 21:12:47 -0500

    no help dm

--- vibes-discord-bot/src/DiscordBot/message/cmds/help.js
+++ vibes-discord-bot/src/DiscordBot/message/cmds/help.js
@@ -43,24 +43,24 @@ export default async function help({ message, cmd_args }) {
 
   const helpMessage = `**I AM VIBES BOT vibesEmoji :robot:** \n
     :eyes: i help you show people's vibes  vibesEmoji \n
-    **DURING A NEW VIBE PERIOD**
+    **DURING A NEW VIBE PERIOD**\n
     :timer: u get a new **\`VIBESTACK\`** at bgn of each **\`VIBEPERIOD\`** \n
     vibedustEmoji  use this react wen fren has **\`!VIBES\`** \n
     susvibesEmoji   use this react wen fren has **\`!susvibes\`** \n
-    **AT THE END OF A VIBE PERIOD**
-    :arrow_right:  vibedustEmoji  ur whole **\`VIBESTACK\`**:pancakes: distros as **\`VIBEDUST\`** based on who you gave \`!VIBES\`
-    :arrow_left:  vibedustEmoji  u get **\`VIBEDUST\`** based on who gave you **\`!VIBES\`** vibesEmoji  you and their \`VIBESTACK\`:pancakes:
-    :mechanical_arm: ur **\`VIBELEVEL\`** is calculated by ranking your **\`VIBEDUST\`** in community
-    **HOW VIBE LEVELS WORK**
-    **\`VIBELEVEL\`**                   **\`BOOST\`**            **\`VIBEDUST % ILE\`**
-    vibesEmoji  \`Frenly Vibe\`           1x                      15.87% - 69.15% 
-    rareEmoji  \`Rare Vibe\`              2x                      69.15% - 84.15% 
-    epicEmoji  \`Epic Vibe\`              3x                      84.15% - 93.32% 
-    legendaryEmoji  \`Legend Vibe\`          4x                      93.32% - 97.72% 
-    ogEmoji  \`OG Vibe\`                   5x                      97.72% - 99.99%
-    :rocket: ur **\`VIBELEVEL\`** will **\`!BOOST\`** ur **\`VIBESTACK\`** before each new **\`VIBEPERIOD\`**
-    **DEEP VIBES**
-    :clipboard: full tx logs on vibescan.io – **vibescan.io/[targetedUser.vibescanID]**
+    **AT THE END OF A VIBE PERIOD**\n
+    :arrow_right:  vibedustEmoji  ur whole **\`VIBESTACK\`**:pancakes: distros as **\`VIBEDUST\`** based on who you gave \`!VIBES\`\n
+    :arrow_left:  vibedustEmoji  u get **\`VIBEDUST\`** based on who gave you **\`!VIBES\`** vibesEmoji  you and their \`VIBESTACK\`:pancakes:\n
+    :mechanical_arm: ur **\`VIBELEVEL\`** is calculated by ranking your **\`VIBEDUST\`** in community\n
+    **HOW VIBE LEVELS WORK**\n
+    **\`VIBELEVEL\`**                   **\`BOOST\`**            **\`VIBEDUST % ILE\`**\n
+    vibesEmoji  \`Frenly Vibe\`           1x                      15.87% - 69.15% \n
+    rareEmoji  \`Rare Vibe\`              2x                      69.15% - 84.15% \n
+    epicEmoji  \`Epic Vibe\`              3x                      84.15% - 93.32% \n
+    legendaryEmoji  \`Legend Vibe\`          4x                      93.32% - 97.72% \n
+    ogEmoji  \`OG Vibe\`                   5x                      97.72% - 99.99%\n
+    :rocket: ur **\`VIBELEVEL\`** will **\`!BOOST\`** ur **\`VIBESTACK\`** before each new **\`VIBEPERIOD\`**\n
+    **DEEP VIBES**\n
+    :clipboard: full tx logs on vibescan.io – **vibescan.io/[targetedUser.vibescanID]**\n
     :eyes: full profiles on vibes.live – **vibes.live/[targetedUser.VibesLiveID]**`.replace(
     /\b(?:vibedustEmoji|vibesEmoji|ogEmoji|rareEmoji|legendaryEmoji|epicEmoji|badvibesEmoji|vibeSender)\b/gi,
     (matched) => nameToEmoji[matched]
@@ -69,8 +69,8 @@ export default async function help({ message, cmd_args }) {
   const helpEmbed = {
     color: 0x00eeee,
     title: `**wat vibes bot?**`,
-    url: `https://www.spot.space/${space.id}`,
-    description: `${helpMessage}\n\n Get lots more vibe analytics on ***spot.space***, click the vibedust to view more!`,
+    url: `https://www.vibesbot.gg`,
+    description: `${helpMessage}`,
     thumbnail: {
       url: "https://media0.giphy.com/media/1fnwSUTsHRyGXEYMos/giphy.gif?cid=ecf05e47b7vkmx00wquadbiunwj71or7xxm1b44iti4zdaxo&rid=giphy.gif&ct=g",
     },
@@ -87,7 +87,7 @@ export default async function help({ message, cmd_args }) {
   const helpEmbedChannel = {
     color: 0x00eeee,
     title: `**wat vibes bot?**`,
-    url: `https://www.spot.space/${space.id}`,
+    url: `https://www.vibesbot.gg`,
     description: `${helpMessageChannel}`,
     thumbnail: {
       url: "https://media2.giphy.com/media/BzM7MRs96dYpLSeUTy/giphy.gif?cid=ecf05e47yk8rvloiy4yh52cdlyzqoil3ksr606xmluc3p6ox&rid=giphy.gif&ct=ts",
@@ -106,7 +106,7 @@ export default async function help({ message, cmd_args }) {
     console.log(e);
   });
 
-  await message.author.send({ embeds: [helpEmbed] }).catch(e => {
-    console.log(e);
-  });
+  // await message.author.send({ embeds: [helpEmbed] }).catch(e => {
+  //   console.log(e);
+  // });
 }
