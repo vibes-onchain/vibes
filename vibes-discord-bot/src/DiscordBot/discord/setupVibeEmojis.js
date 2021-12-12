5de4486732a22bbd2c91217eff8d9f147c48e297
pepimartinez <pepimartinez@protonmail.com>
2021-12-11 19:35:57 -0500

    set emojis function

---
+++ vibes-discord-bot/src/DiscordBot/discord/setupVibeEmojis.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const dirPath = path.join(__dirname, '../img/');
+
+const VIBE_EMOJIS = {
+  "susvibes": { location: `${dirPath}/susvibes.gif`, name: "susvibes" },
+};
+
+export default async function ({ client, guild_id }) {
+  if (!guild_id) {
+    throw new Error("needs guild_id");
+  }
+  const guild = client.guilds.cache.find((g) => g.id === guild_id);
+  if (!guild) {
+    throw new Error("guild not found");
+  }
+  const emoji_names = guild.emojis.cache.map((i) => i.name);
+  for (const emoji of Object.values(VIBE_EMOJIS)) {
+    if (!emoji_names.includes(emoji.name)) {
+      await guild.emojis.create(emoji.location, emoji.name)
+        .then(emoji => console.log(`Created new emoji with name ${emoji.name}!`))
+        .catch(console.error);
+    }
+  }
+}
