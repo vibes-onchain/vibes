1a1772a9a0cbf7dcadf343f2420b46c551470ab6
Foy Savas <foy@sav.as>
2021-12-16 14:38:02 -0500

    null value to 0

--- vibes-discord-bot/src/DiscordBot/spothub/renderVibesUserParen.js
+++ vibes-discord-bot/src/DiscordBot/spothub/renderVibesUserParen.js
@@ -3,11 +3,14 @@ function evalParen(paren, context) {
     paren = "{vibedust}";
   }
   for (const [key, value] of Object.entries(context)) {
-    paren = paren.replaceAll(`{${key}}`, value);
+    paren = paren.replaceAll(
+      `{${key}}`,
+      value !== null && typeof value !== undefined ? value : 0
+    );
   }
   return paren;
 }
 
-export default async function renderParen({paren, context}) {
+export default async function renderParen({ paren, context }) {
   return evalParen(paren, context);
 }
