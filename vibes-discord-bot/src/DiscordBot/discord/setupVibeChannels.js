efe7306d5991d98a0e138dd6a6d5f3eea245d49c
Foy Savas <foy@sav.as>
2021-12-20 17:18:40 -0500

    setup additional vibe channels

--- vibes-discord-bot/src/DiscordBot/discord/setupVibeChannels.js
+++ vibes-discord-bot/src/DiscordBot/discord/setupVibeChannels.js
@@ -1,6 +1,8 @@
 const VIBE_CHANNELS = {
+  "Vibes 101": { name: "vibes-101", type: "GUILD_TEXT" },
+  "Vibenomics": { name: "vibenomics", type: "GUILD_TEXT" },
   "vibe-feed": { name: "vibe-feed", type: "GUILD_TEXT" },
-}
+};
 
 export default async function ({ client, guild_id }) {
   if (!guild_id) {
@@ -10,11 +12,31 @@ export default async function ({ client, guild_id }) {
   if (!guild) {
     throw new Error("guild not found");
   }
+  let vibesCategoryChannel = guild.channels.cache.find(
+    (i) => i.name === "Vibes" && i.type === "GUILD_CATEGORY"
+  );
+  if (!vibesCategoryChannel) {
+    vibesCategoryChannel = await guild.channels.create("Vibes", {
+      type: "GUILD_CATEGORY",
+      permissionOverwrites: [
+        {
+          id: guild_id,
+          allow: ["VIEW_CHANNEL"],
+        },
+      ],
+    });
+  }
   const channel_names = guild.channels.cache.map((i) => i.name);
   for (const channel of Object.values(VIBE_CHANNELS)) {
     if (!channel_names.includes(channel.name)) {
-      await guild.channels.create(channel.name, channel)
-        .then(channel => console.log(`Created new channel with name ${channel.name}!`))
+      await guild.channels
+        .create(channel.name, {
+          ...channel,
+          parent: vibesCategoryChannel.id,
+        })
+        .then((channel) =>
+          console.log(`Created new channel with name ${channel.name}!`)
+        )
         .catch(console.error);
     }
   }
