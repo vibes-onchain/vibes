edac78e9bad42cb5bafec33786429e2990f3fcb6
Foy Savas <foy@sav.as>
2022-01-02 15:17:02 -0500

    setvibesparen

--- vibes-discord-bot/src/DiscordBot/cmds/setvibesparen.js
+++ vibes-discord-bot/src/DiscordBot/cmds/setvibesparen.js
@@ -2,31 +2,45 @@ import LedgerEntry from "spothub/lib/LedgerEntry";
 import findOrCreateLedgerForGuild from "../spothub/findOrCreateLedgerForGuild";
 import canControlVibesBot from "../discord/canControlVibesBot";
 import updateAllGuildMembers from "../multi/updateAllGuildMembers";
+import sendQuickCommandResponse from "../discord/sendQuickCommandResponse";
 
-export default async function setvibesparen({ client, message, cmd_args }) {
-  const message_member = message.member;
-  const guild = message_member.guild;
+export default async function setvibesparen({
+  client,
+  command,
+  message,
+  cmd_args,
+}) {
+  const member = message ? message.member : command.member;
+  const guild = member.guild;
 
   if (
     !(await canControlVibesBot({
       client,
       guild_id: guild.id,
-      member_id: message.member?.id,
+      member_id: member?.id,
     }))
   ) {
     return;
   }
 
-  const space = await findOrCreateLedgerForGuild(guild.id, guild.name);
+  const template = command
+    ? cmd_args.find((i) => i.name === "template").value
+    : cmd_args.join(" ");
+
+  await sendQuickCommandResponse({ command });
+
+  const ledger = await findOrCreateLedgerForGuild(guild.id, guild.name);
   const le = LedgerEntry.build({
-    ledger_id: space.id,
+    ledger_id: ledger.id,
     type: "Set Ledger Metadata",
     value: {
       key: "vibes:paren",
-      value: cmd_args.join(" "),
+      value: template,
     },
   });
   await le.save();
 
   await updateAllGuildMembers({ client, guild_id: guild.id });
+
+  return true;
 }
