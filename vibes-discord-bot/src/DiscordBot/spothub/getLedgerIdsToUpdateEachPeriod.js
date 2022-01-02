bee92b2a9c70454597c020cd025bb14f0ed36b0c
Foy Savas <foy@sav.as>
2022-01-02 10:02:02 -0500

    safer update

--- vibes-discord-bot/src/DiscordBot/spothub/getLedgerIdsToUpdateEachPeriod.js
+++ vibes-discord-bot/src/DiscordBot/spothub/getLedgerIdsToUpdateEachPeriod.js
@@ -5,15 +5,19 @@ export default async function getLedgerIdsToUpdateEachPeriod(period) {
   const ledgers = await Ledger.findAll({ where: {} });
   const r = [];
   for (const ledger of ledgers) {
-    const le = await LedgerEntry.findLast({
-      where: {
-        type: "Set Vibe Period",
-        ledger_id: ledger.id,
-      },
-      order: "desc",
-    });
-    if (le.value?.vibe_period === period) {
-      r.push({ id: ledger.id, guild_id: ledger.meta?.['vibes:discord_guild_id'] });
+    try {
+      const le = await LedgerEntry.findLast({
+        where: {
+          type: "Set Vibe Period",
+          ledger_id: ledger.id,
+        },
+        order: "desc",
+      });
+      if (le.value?.vibe_period === period) {
+        r.push({ id: ledger.id, guild_id: ledger.meta?.['vibes:discord_guild_id'] });
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
   return r;
