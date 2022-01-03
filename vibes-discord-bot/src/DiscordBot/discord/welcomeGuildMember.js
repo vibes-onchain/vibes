1a0cf2ab33dbe1dbd1348b6ba4a2aa73eac123ed
Foy Savas <foy@sav.as>
2022-01-03 12:39:14 -0500

    disable welcome guild member

--- vibes-discord-bot/src/DiscordBot/discord/welcomeGuildMember.js
+++ vibes-discord-bot/src/DiscordBot/discord/welcomeGuildMember.js
@@ -1,9 +1,9 @@
 export default async function welcomeGuildMember(client, guild_id, membe) {
-  const welcomeEmbed = new Discord.MessageEmbed();
+  // const welcomeEmbed = new Discord.MessageEmbed();
 
-  welcomeEmbed.setColor("#5cf000");
-  welcomeEmbed.setTitle("** Welcome Vibes **");
-  welcomeEmbed.setImage("https://i.imgur.com/c76UqNN.gif");
+  // welcomeEmbed.setColor("#5cf000");
+  // welcomeEmbed.setTitle("** Welcome Vibes **");
+  // welcomeEmbed.setImage("https://i.imgur.com/c76UqNN.gif");
 
-  await member.send(welcomeEmbed);
+  // await member.send(welcomeEmbed);
 }
